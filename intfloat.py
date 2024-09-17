b57d8959f39e58f53b2bc68e0e185a00576d476f
Sena Sır <sirsena10@gmail.com>
2024-09-17 20:46:02 +0300

    Add files via upload

---
+++ intfloat.py
@@ -0,0 +1,57 @@
+
+("""sayi1 = 5
+sayi2 = 5.7
+sayi3 = 5 ** 100
+print(type(sayi1))
+print(type(sayi2))  # hangi tip oldugunu anlar.
+print((sayi3))""")
+
+("""sayi = 3.87826
+print(round(sayi,3)) #???*""")
+
+#KARSILASTIRMA
+#print(3 == 3)
+("""sayi1 = 3
+sayi2 = 5
+print(sayi1 > sayi2)""")
+
+#STRİNG VE İNT BİRBİRİNE CEVİRME
+
+("""sayi1 = "100"  #string
+sayi2 = 100     #int
+print(sayi1 == sayi2) #false""")
+
+sayi1 = "100"  #string
+sayi2 = 100    #int
+sayi3 = int(sayi1)      #int degerine cevirdik #inte cevrilebilir olması gerekli
+print(sayi3 == sayi2)
+
+sayi4 = int(3.34)  #sadece sayının tam kısmınnı aldı.
+print(sayi4)       #3
+
+i=30
+i //= 7  #tam sayı bolmesi yapıp i ye atadık.   #3.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
